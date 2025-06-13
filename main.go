0374e3f93160898ee3899577fb2289d0cb5bf6b8
Chamikara Kodithuwakku <chamikaradimuth22@gmail.com>
2025-06-13 11:11:07 +0530

    implement MongoDB connection in config package

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
-  "fmt"
+	"InvoicelyX/config"
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
-
 func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-  }
+	// Connect to MongoDB using config
+	client := config.ConnectDB()
+
+	// Defer closing the connection
+	defer func() {
+		err := client.Disconnect(context.TODO())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Disconnected from MongoDB.")
+	}()
+
+	// Get port from environment variable
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	fmt.Printf("Server ready to start on port %s\n", port)
 }
